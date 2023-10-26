1bd3612619d5fa3a1f974293c34ad5fa6eb834b0
Bartosz Bartosik <babosz189@gmail.com>
2023-10-26 14:38:05 +0200

    Adjusted database initialization code

--- src/main/resources/TutoringDatabase.sql
+++ src/main/resources/TutoringDatabase.sql
@@ -14,6 +14,7 @@ insert into student values(101, 'Dwight', 'Schrute');
 insert into student values(102, 'Michael', 'Scott');
 
 create table lesson(
+    lesson_id int auto_increment,
     student_id int,
     lesson_date date not null,
     start_time time not null,
@@ -23,14 +24,14 @@ create table lesson(
     wage float not null,
     to_pay float,
     payment float not null,
-    constraint ps_lessondate_starttime_pk primary key (lesson_date, start_time),
+    constraint ps_lessonid_pk primary key (lesson_id),
     constraint ps_student_id_fk foreign key(student_id) references student(student_id)
 );
 
-insert into lesson values (100, '2022-11-16', '18:00:00', '19:30:00', '01:30:00', 'Trigonometry', 60.0, 90.0, 100.0);
-insert into lesson values (102, '2022-11-16', '19:45:00', '21:15:00', '01:30:00', 'Quadratic function', 60.0, 90.0, 90.0);
-insert into lesson values (100, '2022-11-18', '18:00:00', '19:30:00', '01:30:00', 'Matrices', 60.0, 90.0, 100.0);
-insert into lesson values (101, '2022-11-18', '20:00:00', '21:30:00', '01:30:00', 'Polynomials', 60.0, 90.0, 0.0);
+insert into lesson values (10, 100, '2022-11-16', '18:00:00', '19:30:00', '01:30:00', 'Trigonometry', 60.0, 90.0, 100.0);
+insert into lesson values (11, 102, '2022-11-16', '19:45:00', '21:15:00', '01:30:00', 'Quadratic function', 60.0, 90.0, 90.0);
+insert into lesson values (12, 100, '2022-11-18', '18:00:00', '19:30:00', '01:30:00', 'Matrices', 60.0, 90.0, 100.0);
+insert into lesson values (13, 101, '2022-11-18', '20:00:00', '21:30:00', '01:30:00', 'Polynomials', 60.0, 90.0, 0.0);
 
 commit;
 select * from student;
