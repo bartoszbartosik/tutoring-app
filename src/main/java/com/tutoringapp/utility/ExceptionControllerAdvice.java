0a9961cc87b8ab266012fc26b4686f05ea49318f
Bartosz Bartosik <babosz189@gmail.com>
2023-10-26 16:07:32 +0200

    Created ErrorInfo class for storing error information; Created Rest Controller Advice class for exception handling

---
+++ src/main/java/com/tutoringapp/utility/ExceptionControllerAdvice.java
@@ -0,0 +1,30 @@
+package com.tutoringapp.utility;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.tutoringapp.exception.TutoringAppException;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+	
+	@Autowired
+	Environment environment;
+	
+	@ExceptionHandler(TutoringAppException.class)
+	public ResponseEntity<ErrorInfo> tutoringAppExceptionHandler(TutoringAppException exception) {
+		ErrorInfo error = new ErrorInfo();
+		error.setErrorMessage(environment.getProperty(exception.getMessage()));
+		error.setErrorCode(HttpStatus.NOT_FOUND.value());
+		error.setTimestamp(LocalDateTime.now());
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	
+}
