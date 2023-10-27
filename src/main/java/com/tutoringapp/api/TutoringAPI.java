5ca6b472a06f610d0652aaaac74e5cea93e4b42c
Bartosz Bartosik <babosz189@gmail.com>
2023-10-27 18:07:20 +0200

    Created endpoint in the API layer

--- src/main/java/com/tutoringapp/api/TutoringAPI.java
+++ src/main/java/com/tutoringapp/api/TutoringAPI.java
@@ -15,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tutoringapp.dto.LessonDTO;
 import com.tutoringapp.dto.StudentDTO;
 import com.tutoringapp.exception.TutoringAppException;
+import com.tutoringapp.service.LessonService;
 import com.tutoringapp.service.StudentService;
 
 @RestController
@@ -27,9 +29,14 @@ public class TutoringAPI {
 	private StudentService studentService;
 	
 	@Autowired
+	private LessonService lessonService;
+	
+	@Autowired
 	private Environment environment;
 	
-	
+	///////////////////////////
+	////   S T U D E N T   ////
+	///////////////////////////
 	@GetMapping(value = "/students")
 	public ResponseEntity<List<StudentDTO>> getAllStudents() throws TutoringAppException {
 		List<StudentDTO> studentsList = studentService.findAll();
@@ -63,5 +70,14 @@ public class TutoringAPI {
 		return new ResponseEntity<>(successMessage, HttpStatus.OK);
 	}
 	
+
+	///////////////////////////
+	/////   L E S S O N   /////
+	///////////////////////////
+	@GetMapping(value = "/lessons")
+	public ResponseEntity<List<LessonDTO>> getAllLessons() throws TutoringAppException {
+		List<LessonDTO> lessonsList = lessonService.findAll();
+		return new ResponseEntity<>(lessonsList, HttpStatus.OK);
+	}
 	
 }
