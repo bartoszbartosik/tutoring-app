0d1fc356e8e17dad4efb82004a1253b5aed415be
Bartosz Bartosik <babosz189@gmail.com>
2023-10-27 16:26:38 +0200

    Set up LessonService and LessonServiceImpl classes

--- src/main/java/com/tutoringapp/service/LessonService.java
+++ src/main/java/com/tutoringapp/service/LessonService.java
@@ -1,4 +1,10 @@
 package com.tutoringapp.service;
 
+import java.util.List;
+
+import com.tutoringapp.entity.Lesson;
+import com.tutoringapp.exception.TutoringAppException;
+
 public interface LessonService {
+	public List<Lesson> findAll() throws TutoringAppException;
 }
--- src/main/java/com/tutoringapp/service/LessonServiceImpl.java
+++ src/main/java/com/tutoringapp/service/LessonServiceImpl.java
@@ -1,4 +1,25 @@
 package com.tutoringapp.service;
 
-public class LessonServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tutoringapp.entity.Lesson;
+import com.tutoringapp.exception.TutoringAppException;
+import com.tutoringapp.repository.LessonRepository;
+
+@Service(value = "lessonService")
+@Transactional
+public class LessonServiceImpl implements LessonService {
+	
+	@Autowired
+	private LessonRepository lessonRepository;
+
+	@Override
+	public List<Lesson> findAll() throws TutoringAppException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
